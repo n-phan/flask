355df0631a000bdefda342eaf7c5e75b839b6a78
nathanphan <nathan.hd.phan@gmail.com>
2019-12-27 12:39:17 -0800

    added profile page, avatar images and profile editing

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,14 +1,16 @@
 from datetime import datetime
 from app import db, login
-from flask_login import UserMixin
+from flask_login import UserMixin # UserMixin, class that includes generic implementations of methods needed for Flask-Login
 from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5 # User avatar generator
 
-# UserMixin, class that includes generic implementations of methods needed for Flask-Login
 class User(UserMixin, db.Model): 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def set_password(self, password):
@@ -16,6 +18,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)

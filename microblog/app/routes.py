618ab1e3c8b2d689b3b0dcdc5854477498277901
nathanphan <nathan.hd.phan@gmail.com>
2019-12-27 09:58:38 -0800

    flask webforms, flask-wtf, validation

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import application
+from app.forms import LoginForm
 
 @application.route('/')
 @application.route('/index')
@@ -17,6 +18,11 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-# def index():
-#     user = {'username': 'ayophoenix'}
-#     return render_template('index.html', title='Home', user=user)
+@application.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
